0642beffe5c6ed29b25bf8ea56c5661fb89fd4c2
4alkubati <4alkubati@gmail.com>
2026-05-05 09:47:49 -0600

    init: SwingBy monorepo scaffold

---
+++ research/frontend-map.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="Freemind_Link_1513112588" name="Root" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="SWING BY" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="Freemind_Link_1513112588" CREATED="1153430895318" MODIFIED="1765826129637" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" CHILD_NODES_LAYOUT="AUTO">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#ffffffff" zoom="1.1">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="false" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_purple_neon_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1102549087" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2b0053" BACKGROUND_COLOR="#ff4fdb" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#585858" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1102549087" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#8001dd" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ff4fdb" BACKGROUND_COLOR="#2b0053">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffff9e" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#ffec56"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2b0053" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#fd004c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#fd004c">
+<icon BUILTIN="yes"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#fd004c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#8001dd" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+<edge COLOR="#ff4fdb"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#a120f2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#c100fe" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#dd00fd" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#282828" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Communication" POSITION="bottom_or_right" ID="ID_1978809397" CREATED="1765826296912" MODIFIED="1765826461152">
+<node TEXT="" ID="ID_281831276" CREATED="1765826321087" MODIFIED="1765826321087"/>
+<node TEXT="" ID="ID_279003983" CREATED="1765826375024" MODIFIED="1765826375024"/>
+</node>
+<node TEXT="Category Coverage" POSITION="top_or_left" ID="ID_523957623" CREATED="1765826298014" MODIFIED="1765853516542">
+<node TEXT="" ID="ID_143321063" CREATED="1765826328188" MODIFIED="1765826328188"/>
+<node TEXT="" ID="ID_504559404" CREATED="1765826380710" MODIFIED="1765826380710"/>
+</node>
+<node TEXT="Trust &amp; Reviews" POSITION="bottom_or_right" ID="ID_1389774075" CREATED="1765826303180" MODIFIED="1765826462780">
+<node TEXT="" ID="ID_233038763" CREATED="1765826374114" MODIFIED="1765826374114"/>
+</node>
+<node TEXT="User Experience" POSITION="top_or_left" ID="ID_1215554755" CREATED="1765826305557" MODIFIED="1766113492989" VGAP_QUANTITY="3 pt">
+<node TEXT="" ID="ID_1278447517" CREATED="1765826382704" MODIFIED="1766113492973"/>
+<node TEXT="" ID="ID_1025713843" CREATED="1765826383932" MODIFIED="1765826383932"/>
+</node>
+<node TEXT="Payments" POSITION="bottom_or_right" ID="ID_1596434833" CREATED="1765826312801" MODIFIED="1765826472316">
+<node TEXT="" ID="ID_675452745" CREATED="1765826321839" MODIFIED="1765826321839"/>
+<node TEXT="" ID="ID_1933815905" CREATED="1765826324753" MODIFIED="1765826324753"/>
+</node>
+<node TEXT="Booking &amp; Scheduling" POSITION="top_or_left" ID="ID_1811977166" CREATED="1765826313923" MODIFIED="1765826438756">
+<node TEXT="" ID="ID_1335075660" CREATED="1765826385025" MODIFIED="1765826385025"/>
+<node TEXT="" ID="ID_1514836420" CREATED="1765826386010" MODIFIED="1765826386010"/>
+</node>
+<node TEXT="User Experience" POSITION="bottom_or_right" ID="ID_1582255490" CREATED="1765826314768" MODIFIED="1765826479126">
+<node TEXT="" ID="ID_741578529" CREATED="1765826346058" MODIFIED="1765826346058"/>
+<node TEXT="" ID="ID_1763558814" CREATED="1765826347079" MODIFIED="1765826347079"/>
+</node>
+<node TEXT="Pricing" POSITION="top_or_left" ID="ID_309309998" CREATED="1765826315758" MODIFIED="1765826446844">
+<node TEXT="" ID="ID_1327394506" CREATED="1765826387075" MODIFIED="1765826387075"/>
+<node TEXT="" ID="ID_1586916422" CREATED="1765826387565" MODIFIED="1765826387565"/>
+</node>
+<node TEXT="Category Coverage" POSITION="bottom_or_right" ID="ID_346637134" CREATED="1765826316514" MODIFIED="1765826484081">
+<node TEXT="" ID="ID_281879340" CREATED="1765826342990" MODIFIED="1765826342990"/>
+<node TEXT="" ID="ID_762730264" CREATED="1765826343946" MODIFIED="1765826343946"/>
+</node>
+</node>
+</map>
